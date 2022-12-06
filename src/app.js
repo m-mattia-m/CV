d2aff2ffafaff87896fee1cb3015e2435cb13d07
m-mattia-m <59088904+m-mattia-m@users.noreply.github.com>
2022-12-06 22:44:16 +0100

    add structure and language-toggle

--- src/app.js
+++ src/app.js
@@ -1,2 +1,1 @@
 import './components/home'
-import './components/form'
--- src/components/home.js
+++ src/components/home.js
@@ -1,58 +1,84 @@
 import { getSectionCordinates, scrollTo } from "../helpers/scroll";
+import { setGerman, setEnglish } from "../helpers/language.js";
 
 window.homeComponent = () => {
+
+    /**
+     * @type string
+     */
+    let chosenLanguage = "en";
+    /**
+     * returnLanguage
+     * @return string
+     */
+    function returnLanguage(jsonPath){
+        console.log("lang: " + chosenLanguage)
+        if (chosenLanguage == "en") {
+            return setEnglish(jsonPath);
+        }
+        if (chosenLanguage == "de") {
+            return setGerman(jsonPath);
+        }
+        return "";
+    };
+
     return {
-        /**
-         * onReadMoreClick
-         * @param {MouseEvent} $event
-         * @param {HTMLElement} section
-         * @param {HTMLElement} header
-         */
-        onReadMoreClick($event, section, header) {
-            const sectionCordinates = getSectionCordinates(section);
-            const headerHeight = header.clientHeight;
-            const padding = 30;
-
+        language: {
             /**
-             * @type ScrollToOptions
+             * @type string
              */
-            const scrollToOptions = {
-                left: 0,
-                top: sectionCordinates.top - headerHeight - padding,
-            };
-
-            scrollTo(scrollToOptions);
-        },
-        /**
-         * onLogoClick
-         * @param {MouseEvent} $event
-         */
-        onLogoClick($event) {
-            scrollTo({
-                left: 0,
-                top: 0,
-            });
-        },
-        /**
-         * onNavLinkClick
-         * @param {MouseEvent} $event
-         * @param {HTMLElement} section
-         * @param {HTMLElement} header
-         */
-        onNavLinkClick($event, section, header) {
-            const sectionCordinates = getSectionCordinates(section);
-            const headerHeight = header.clientHeight;
-            const padding = 30;
-
+            sectionOne: returnLanguage("title-section-1"),
             /**
-             * @type ScrollToOptions
+             * @type string
              */
-            const scrollToOptions = {
-                left: 0,
-                top: sectionCordinates.top - headerHeight - padding,
-            };
-
-            scrollTo(scrollToOptions);
+            sectionTwo: returnLanguage("title-section-2"),
+            /**
+             * @type string
+             */
+            sectionThree: returnLanguage("title-section-3"),
+            /**
+             * @type string
+             */
+            sectionFour: returnLanguage("title-section-4"),
+            /**
+             * @type string
+             */
+            sectionFive: returnLanguage("title-section-5"),
+            /**
+             * @type string
+             */
+            sectionSix: returnLanguage("title-section-6"),
         },
+
+        /**
+         * setText
+         * */
+        setText(){
+            this.language.sectionOne = returnLanguage("title-section-1");
+            this.language.sectionTwo = returnLanguage("title-section-2");
+            this.language.sectionThree = returnLanguage("title-section-3");
+            this.language.sectionFour = returnLanguage("title-section-4");
+            this.language.sectionFive = returnLanguage("title-section-5");
+            this.language.sectionSix = returnLanguage("title-section-6");
+
+            console.log("set-text: " + this.language.sectionTwo);
+        },
+        /**
+         * toggleLang
+         * @param {MouseEvent} $event
+         */
+        toggleLang($event){
+            console.log("old lang: " + chosenLanguage);
+            if (chosenLanguage == "de"){
+                chosenLanguage = "en";
+            } else if (chosenLanguage == "en"){
+                chosenLanguage = "de";
+            }
+            console.log("new lang: " + chosenLanguage);
+            console.log(returnLanguage("title-section-1"));
+            this.setText();
+        },
+
+
     };
 };
