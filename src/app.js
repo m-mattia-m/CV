b2f4991ac3f4d2b7b8a9b2036a19c6fb157de0f0
m-mattia-m <59088904+m-mattia-m@users.noreply.github.com>
2022-12-17 22:27:33 +0100

    move dynamic image import

--- src/app.js
+++ src/app.js
@@ -1,3 +1,2 @@
 import './components/home'
 
-import imageUrl from '/assets/images/bookspread.png'
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import Alpine from 'alpinejs';
 // styles
 import './style.scss';
 
+// assets
+import imageUrl from '/assets/images/bookspread.png'
+
 const initApp = () => {
     window.Alpine = Alpine;
     Alpine.start();
