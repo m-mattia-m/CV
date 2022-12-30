b3c66308ff6bb536bf8aad6627e04bfb09faa792
m-mattia-m <59088904+m-mattia-m@users.noreply.github.com>
2022-12-30 10:17:22 +0100

    change image-tag-value and export images

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import bookspread from '/assets/images/bookspread.png';
 import crm from '/assets/images/crm.png';
 import moebilo from '/assets/images/moebilo.png';
 
+export { bookspread, crm, moebilo };
+
 const initApp = () => {
     window.Alpine = Alpine;
     Alpine.start();
